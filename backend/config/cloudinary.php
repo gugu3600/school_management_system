9783ffb43e2be69cf205002cada06ab9f0ff2c5b
gugu3600 <112836387+gugu3600@users.noreply.github.com>
2026-02-28 14:34:01 +0630

    Refactor Cloudinary config with default values
    Updated Cloudinary configuration to use default values.

--- backend/config/cloudinary.php
+++ backend/config/cloudinary.php
@@ -1,13 +1,11 @@
 <?php
 return [
-<<<<<<< HEAD
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key'    => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
 ];
-=======
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'dxuxc086g'),
     'api_key'    => env('CLOUDINARY_KEY', '655361173278156'),
     'api_secret' => env('CLOUDINARY_SECRET', 'AzEPaRGnWZLzfTkCW-Pn3U03x20'),
 ];
->>>>>>> e27fd7aca47427a1c5162e6f3cef338b282748fb
+
